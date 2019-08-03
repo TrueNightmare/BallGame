cc3ea6de97f3db419a58ee389aa1588310561e74
TrueComputer <c-summerhill@hotmail.com>
2019-08-03 21:19:38 +0100

    First Push
    2 Levels, Basic game play and player detection, will do more tomorrow

---
+++ BallGame/Assets/Scripts/s_camera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_camera : MonoBehaviour
+{
+    public GameObject ViewTarget;
+    public bool FollowTarget;
+
+    public Vector3 Differnce;
+
+    [Tooltip("View Target as pivot point, useless if FollowTarget is enabled")]
+    public bool LookAtTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Differnce = transform.position - ViewTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (FollowTarget)
+        {
+            transform.position = new Vector3(ViewTarget.transform.position.x + Differnce.x,
+                ViewTarget.transform.position.y + Differnce.y,
+                ViewTarget.transform.position.z + Differnce.z);
+        }
+
+        if (LookAtTarget)
+        {
+            transform.LookAt(ViewTarget.transform);
+        }
+    }
+}
---
+++ BallGame/Assets/Scripts/s_worldmover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_worldmover : MonoBehaviour
+{
+    [Header("Degree Limits")]
+    public float MaxX = 35;
+    public float MaxZ = 35;
+
+    float x;
+    float z;
+    private Quaternion Euler;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        x = Input.GetAxis("Horizontal") * MaxX;
+        z = Input.GetAxis("Vertical") * MaxZ;
+
+        Euler = Quaternion.Euler(x, 0, z);
+
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Euler, Time.deltaTime * 2);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, Time.deltaTime * 2);
+        }
+    }
+}
