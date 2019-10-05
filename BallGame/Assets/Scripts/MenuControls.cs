a3140eba05f134f2bd223d24138abf84efca34fb
Laptop <c-summerhill@hotmail.com>
2019-10-05 17:02:38 +0100

    MenuSettings
    Updating Settings, Still work to go but its working

--- BallGame/Assets/Scripts/MenuControls.cs
+++ BallGame/Assets/Scripts/MenuControls.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class MenuControls : MonoBehaviour
 {
+    [Header("Event Handler")]
+    public EventSystem EventSystem;
+
     [Header("Canvas")]
     public Canvas Main;
     public Canvas Settings;
@@ -18,10 +22,25 @@ public class MenuControls : MonoBehaviour
     public void QuitApplication()
     {
         Application.Quit();
+        Debug.Log("Button Pressed");
     }
 
-    public void MainToSettings(GameObject NextSelected)
+    public void MainToSettings()
     {
+        Main.enabled = false;
+        Settings.enabled = true;
+        EventSystem.firstSelectedGameObject = SettingsButton;
+    }
 
+    public void SettingsToMain()
+    {
+        Settings.enabled = false;
+        Main.enabled = true;
+        EventSystem.firstSelectedGameObject = MainButton;
+    }
+
+    public void SetGraphic(int Quailtiy)
+    {
+        UnityEngine.QualitySettings.SetQualityLevel(Quailtiy);
     }
 }
